ae4f81fe95be3fd55195f78a694c09a50e45406b
FedorivO <44712938+FedorivO@users.noreply.github.com>
2020-05-02 15:24:16 +0300

    Add files via upload

---
+++ create.sql
@@ -0,0 +1,36 @@
+CREATE TABLE State (
+    state_name VARCHAR(20) NOT NULL PRIMARY KEY
+);
+
+
+
+CREATE TABLE City (
+  state_name VARCHAR(20) NOT NULL REFERENCES State(state_name),
+  city_name VARCHAR(20) NOT NULL PRIMARY KEY
+);
+
+
+
+CREATE TABLE Criminal (
+  city_name VARCHAR(20) NOT NULL REFERENCES City(city_name),
+  shoot_id NUMBER(10, 0) NOT NULL,
+  name VARCHAR(50) NOT NULL,
+  date_ timestamp,
+  manner_of_death VARCHAR(50),
+  armed VARCHAR(50),
+  age NUMBER(3, 0) NOT NULL,
+  city_id varchar(50) NOT NULL PRIMARY KEY,
+  gender_name VARCHAR(6) NOT NULL
+);
+
+CREATE TABLE Person (
+    name VARCHAR(50) NOT NULL,
+    date_ timestamp PRIMARY KEY,
+    manner_of_death VARCHAR(50),
+    age NUMBER(3, 0) NOT NULL,
+    gender_name VARCHAR(6) NOT NULL
+);
+
+CREATE TABLE Gender (
+    gender_name VARCHAR(6) NOT NULL PRIMARY KEY
+);
