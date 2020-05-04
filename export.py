f1bfcb6923677524aa52b43c01a5d0d91238daa6
FedorivO <44712938+FedorivO@users.noreply.github.com>
2020-05-04 12:45:43 +0300

    Add files via upload

---
+++ export.py
@@ -0,0 +1,29 @@
+import cx_Oracle
+import csv
+
+username = 'user2'
+password = 'user2'
+database = 'localhost'
+
+connection = cx_Oracle.connect(username,password, database)
+cursor = connection.cursor()
+
+
+tables = ['State', 'City', 'Criminal', 'Person']
+
+for table in tables:
+    with open(f'{table}.csv', 'w', newline='') as file:
+        writer = csv.writer(file, delimiter=',')
+
+        query = f'SELECT * FROM {entity}'
+        cursor.execute(query)
+
+        row = cursor.fetchone()
+         while row:
+            writer.writerow(row)
+            row = cursor.fetchone()
+        
+
+
+cursor.close()
+connection.close()
