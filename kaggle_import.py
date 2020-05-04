eb054ce29f664c92b06a94f9b8c3205fd17ef2fd
FedorivO <44712938+FedorivO@users.noreply.github.com>
2020-05-04 18:07:50 +0300

    Update kaggle_import.py

--- kaggle_import.py
+++ kaggle_import.py
@@ -8,6 +8,7 @@ database = 'localhost'
 row_num = 1
 states = []
 cities = []
+dates = []
 
   
 connection = cx_Oracle.connect(username,password, database)
@@ -46,12 +47,13 @@ try:
             cities_query = "INSERT INTO City (state_name, city_name) VALUES (:state_name, :city_name)"
             cursor.execute(cities_query, state_name=state, city_name=city)
             cities.append(city)
-        
+         
+        if date_ not in dates:
+            dates_query = "INSERT INTO Person (name, date_,manner_of_death,age,gender_name) 
+            VALUES (:name, TO_DATE(:date_,'yyyy-mm-dd'), :manner_of_death, :age, :gender_name)"
+            cursor.execute(dates_query, name=name, date_=date_ , manner_of_death=manner_of_death, age=age, gender_name=gender_name)
+            dates.append(date_)
 
- 
-        insert_query = """INSERT INTO Person(name, date_,manner_of_death,age,gender_name)
-            VALUES (:name, TO_DATE(:date_,'yyyy-mm-dd'), :manner_of_death, :age, :gender_name)"""
-        cursor.execute(insert_query, name=name, date_=date_ , manner_of_death=manner_of_death, age=age, gender_name=gender_name)
         
         insert_query = """INSERT INTO Criminal(city_name, shoot_id, name, date_, manner_of_death, armed, age, gender_name)
             VALUES (:city_name, :shoot_id, :name, TO_DATE(:date_,'yyyy-mm-dd'), :manner_of_death, :armed, :age, :gender_name)"""
